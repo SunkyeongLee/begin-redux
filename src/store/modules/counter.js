9799bc1d6b9887497c60915311302bb43919aa43
Sunkyeong Lee <sunkyong9768@gmail.com>
2020-03-04 17:48:57 +0900

    studying redux

--- src/store/modules/counter.js
+++ src/store/modules/counter.js
@@ -1,1 +1,23 @@
 // 카운터 관련 상태 로직
+import { createAction, handleActions } from 'redux-actions';
+
+const INCREMENT = 'counter/INCREMENT';
+const DECREMENT = 'counter/DECREMENT';
+
+// create action function
+export const increment = createAction(INCREMENT);
+export const decrement = createAction(DECREMENT);
+
+// set default
+const initialState = {
+    number: 0
+};
+
+// 액션함수 생성; reducer
+export default handleActions({
+    [INCREMENT]: (state, action) => {
+        return { number: state.number + 1 };
+    },
+    //비구조화 할당
+    [DECREMENT]: ({ number }) => ({ number: number - 1 })
+}, initialState);
--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -1,1 +1,8 @@
+import { combineReducers } from 'redux';
+import counter from './counter';
+
 // 모든 모듈들을 불러와서 합치는 작업이 이뤄짐
+
+export default combineReducers({
+    counter
+});
